6e35f04b7d4667a3b830e47e5d48d877e8c0541e
Neilio3264 <neilrathod12@gmail.com>
2020-10-11 03:13:30 -0500

    add firestore integration with server calculation logic

--- client/functions/index.js
+++ client/functions/index.js
@@ -1,7 +1,36 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase/app');
+require('firebase/firestore');
 const roundrobinsort = require('./roundrobin');
 const { response } = require('express');
 const express = require('express');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCdatZG0-muSXWTZrPsN29aIj1v-52YvVc',
+  authDomain: 'table-tennis-vs.firebaseapp.com',
+  databaseURL: 'https://table-tennis-vs.firebaseio.com',
+  projectId: 'table-tennis-vs',
+  storageBucket: 'table-tennis-vs.appspot.com',
+  messagingSenderId: '682006034824',
+  appId: '1:682006034824:web:5fdf58b3dc12c1d62f223d',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.firestore();
+
+async function getPlayers(db) {
+  var players = [];
+  const playersSnap = await db
+    .collection('clubs')
+    .doc('utd')
+    .collection('Player')
+    .orderBy('elo', 'desc')
+    .get();
+  playersSnap.forEach((doc) => {
+    players.push(doc.data().getString('name'));
+  });
+  return players;
+}
 
 var router = express.Router();
 
@@ -13,11 +42,11 @@ app.get('/', (request, response) => {
 });
 
 router.get('/leagues', function (request, response, next) {
-  response.json([generateLeague(players, numPlayers)]);
+  response.json([generateLeague(getPlayers(db), numPlayers)]);
 });
 
 router.get('/matches', function (request, response, next) {
-  response.json([shufflePlayers(players)]);
+  response.json([shufflePlayers(getPlayers(db))]);
 });
 
 app.listen(port, () => {
