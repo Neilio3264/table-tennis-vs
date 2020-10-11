9c2c6a75ed640760fb8944ce292f96715586c70f
Tim Tersigni <tim.tersigni@gmail.com>
2020-10-10 21:18:06 -0500

    Use PlayerList and Player components in LeaguePanel.

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import Player from './Player'
+import PropTypes from 'prop-types'
 
 export class PlayerList extends Component {
-render() {
-  return this.props.panels.map((panel) => (
-    <LeaguePanel key={panel.id} title={panel.title} players={panel.players}/>
-    ));
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.players.map((player) => {
+                return <li>
+                  <Player name={player} elo={0}/>
+                </li>
+              })}
+        </ul>
+      </div>
+    )
   }
+}
+
+
+// PropTypes
+PlayerList.propTypes = {
+  players: PropTypes.array.isRequired
 }
 
 export default PlayerList
